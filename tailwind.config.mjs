489f0974d717245b41b834afd4769a66d2368351
VixoPlank <vichevasquez@gmail.com>
2024-06-09 09:45:57 -0400

    add animation for text gradient

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				'text-gradient': 'text-gradient 3s linear infinite'
+			},
+			keyframes: {
+				'text-gradient': {
+					to: {
+						backgroundPosition: '200% center'
+					}
+				}
+			}
+		}
 	},
-	plugins: [],
+	plugins: []
 }
