35b6f6de0a6e95dfe1822fb62dd320c5d4d2d8c2
VixoPlank <vichevasquez@gmail.com>
2026-01-10 21:01:33 -0300

    feat: integrate Vercel serverless adapter and update dependencies
    - Added Vercel serverless adapter to astro.config.mjs for deployment support.
    - Updated package.json to include @astrojs/vercel dependency.
    - Modified pnpm-lock.yaml to reflect changes in dependencies and ensure compatibility.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
   integrations: [tailwind(), react(), markdoc(), keystatic()]
 });
